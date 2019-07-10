4aa6fcfcf344e7e22304847e3041ce5f0d8831e3
gsoykan <gurkan@naylalabs.com>
2019-07-11 00:40:55 +0300

    Refactor on SWT-39

--- Sources/App/API/Context/EventContext.swift
+++ Sources/App/API/Context/EventContext.swift
@@ -4,4 +4,11 @@ import Vapor
 struct EventContext: Content {
     var videos: [Video]
     var event: Event
+    
+    init(videos: [Video], event: Event ) {
+        videos.forEach{ $0.setIsUpComing() }
+        self.videos = videos
+        self.event = event
+    }
+    
 }
